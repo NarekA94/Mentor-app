318ce339b69020aafcad7f61e9e275f9bda98bbb
Narek <narek@MacBook-Pro.local>
2021-01-27 12:31:06 +0400

    add safearea provider

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import {HomeScreen} from '../screens';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 export default () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Home" component={HomeScreen} />
-  </Stack.Navigator>
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </SafeAreaProvider>
 );
